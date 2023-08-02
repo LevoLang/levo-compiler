3041cc3ec88e6cad6ff93d40c68035e7a3114765
Ahraman <ahraman12000@gmail.com>
2023-08-03 03:28:20 +0330

    Moved the content of main() to a test unit.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,2 @@
 fn main() {
-    use levoc::parse::lex::*;
-
-    let mut lexer = create_lexer("a+b*(c-d)".chars());
-    while let Ok(tok) = lexer.lex() {
-        println!("{}", tok);
-    }
 }
---
+++ tests/str-lexer-test.rs
@@ -0,0 +1,17 @@
+#[test]
+fn str_lexer_test() {
+    use levoc::parse::lex::{Lex, Lexer};
+
+    let texts = [
+        "abc32+bec_*(c232d-d_3u2s)",
+    ];
+
+    for (num, text) in texts.iter().enumerate() {
+        println!("==========");
+        println!("Test #{}:", num.to_string());
+        let mut lexer = Lexer::new(text.chars());
+        while let Ok(tok) = lexer.lex() {
+            println!("{}", tok);
+        }
+    }
+}
